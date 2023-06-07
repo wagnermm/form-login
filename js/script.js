e14fd3e90c54f989698f79eaf10e8899fdf5dbc5
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-06-07 11:10:38 -0300

    alterando cores/removendo vendor

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
-function validate(elem) {
-    if(elem.val() == '' ) {
-        elem.parent().find('.text-muted').show()
-        return false
-    } else {
-        elem.parent().find('.text-muted').hide()    
-    }
-}
 
-$('.form').on('submit', function(e) {
-    e.preventDefault()
-    const inputName = $('#name')
-    validate(inputName)
-    if(inputName.hasClass('invalid')) {
-        console.log('Verificar os campos obrigatóios')
-        return false
-    } else {
-        $(this).submit()
-    }
-})
+function validaName(elemento){
+    elemento.addEventListener('focusout', function() {
+        const nameValido = /^[a-z]{2}/i
+        if(this.value.match(nameValido)){
+            document.querySelector('#messageName').innerHTML = "";
+            document.querySelector('.btn').disabled = false;
+            return false;
+        } else {
+            document.querySelector('#messageName').innerHTML = "Invalid Name";
+            document.querySelector('.btn').disabled = true;
+        };
+    });
+  }
+  let campoName = document.querySelectorAll('input#name');
+  for( let emFoco of campoName) {
+      validaName(emFoco);
+  }
 
-$('.form').on('blur', '#name', function() {
-    validate($(this))   
-})
+
+  function validaPass(elemento){
+    elemento.addEventListener('focusout', function() {
+        const passValido = /^[0-9]{4}$/
+        if(this.value.match(passValido)){
+            document.querySelector('#messagePass').innerHTML = "";
+            document.querySelector('.btn').disabled = false;
+            return false;
+        } else {
+            document.querySelector('#messagePass').innerHTML = "Your password must have 4 numbers";
+            document.querySelector('.btn').disabled = true;
+        };
+    });
+  }
+  let campoPass = document.querySelectorAll('input#password');
+  for( let emFoco of campoPass) {
+      validaPass(emFoco);
+  }
