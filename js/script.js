0943bae6c4ef8236ce1b69d19bceef8328f6c63a
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-07-20 11:44:36 -0300

    setup with js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ function validaName(elemento){
         const nameValido = /^[a-z]{2}/i
         if(this.value.match(nameValido)){
             document.querySelector('#messageName').innerHTML = "";
-            document.querySelector('.btn').disabled = false;
+            document.querySelector('.btn').disabled = true;
             return false;
         } else {
             document.querySelector('#messageName').innerHTML = "Invalid Name";
             document.querySelector('.btn').disabled = true;
         };
     });
-  }
+  };
   let campoName = document.querySelectorAll('input#name');
   for( let emFoco of campoName) {
       validaName(emFoco);
-  }
+  };
 
 
   function validaPass(elemento){
@@ -30,8 +30,20 @@ function validaName(elemento){
             document.querySelector('.btn').disabled = true;
         };
     });
-  }
+  };
   let campoPass = document.querySelectorAll('input#password');
   for( let emFoco of campoPass) {
       validaPass(emFoco);
+  };
+
+  function btnEnter() {
+    let nome = document.querySelector('#name').value;
+    let password = document.querySelector('#password').value;
+    if(nome && password) {
+        document.querySelector('.btn').disabled = false;
+        document.querySelector('.container').style.display="none"
+    } else {
+        document.querySelector('.btn').disabled = true;
+        document.querySelector('.container').style.display="block"
+    }
   }
